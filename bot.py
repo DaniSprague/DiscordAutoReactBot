813afdb27a8e705ea45854bcaff52275a89e2cf1
Dani Sprague <danielsprague@wavecable.com>
2020-04-21 16:47:25 -0700

    Refactor/comment bot.py
    Moved parts of on_message() to their own methods, with improved commenting. Also changed how some of the printing to console works in reacting to messages.

--- bot.py
+++ bot.py
@@ -15,30 +15,46 @@ client = discord.Client()
 
 user_emojis = dict()
 
+
 @client.event
 async def on_ready():
     """The main event once the bot connects to Discord."""
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!\n')
+
 
 @client.event
 async def on_message(message):
     """Either sets the user's emoji's preferences, or reacts to a message"""
-    #Message must have contents "!Autoreact.set {Preferred-Emoji}"
     if message.content[:15] == "!AutoReact.set ":
-        user_emojis[message.author] = message.content[15:16]
+        await _set_pref(message)
     else:
-        try:
-            emoji = user_emojis.get(message.author, None)
-            if emoji is not None:
-                await message.add_reaction(emoji)
-        except Exception as e:
-            print(f"{datetime.datetime.now()}: error reacting")
-            raise
-        else:
-            print(
-                f"{datetime.datetime.now()}: Reacted to {message.author}'s message",
-                f"in guild {message.guild} in channel {message.channel} with",
-                f"{emoji}")
+        await _react(message)
 
+
+async def _set_pref(message):
+    """Sets a user's preference for their reaction emoji.
+
+    Accepts an argument of a Discord message of format "!Autoreact.set {emoji}"
+    Takes the emoji and sets that to be the user's preferred emoji.
+    """
+    user_emojis[message.author] = message.content[15:16]
+
+
+async def _react(message):
+    """Reacts to the given emoji with the user's preferred emoji.
+
+    Accepts an argument of a Discord message to be reacted to.
+    Checks the author's preference of emoji and reacts.
+    """
+    try:
+        emoji = user_emojis.get(message.author, None)
+        if emoji is not None:
+            await message.add_reaction(emoji)
+            print(
+                f"{datetime.datetime.now()}: Reacted to {message.author}'s",
+                f"message with {emoji}.")
+    except Exception as e:
+        print(f"{datetime.datetime.now()}: error reacting")
+        raise
 
 client.run(TOKEN)
