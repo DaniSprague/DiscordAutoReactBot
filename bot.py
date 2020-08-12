73ed367a7a4e90475fc62407d7bc269095985a12
Dani Sprague <DaniJSprague@gmail.com>
2020-08-12 13:27:57 -0700

    Implement logging from discord.py documentation

--- bot.py
+++ bot.py
@@ -12,9 +12,20 @@ This script depends on the discord and dotenv modules. See README.md for more.
 import datetime
 import os
 import json
+import logging
 
 import discord
 from dotenv import load_dotenv
+
+# Logging set-up (basic logging code from the discord.py documentation)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log',
+                              encoding='utf-8',
+                              mode='a')
+handler.setFormatter(
+    logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = discord.Client()
 
